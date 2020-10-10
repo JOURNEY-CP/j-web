1a2f92eeb9525be978cf9e5f666bb81a37401b0a
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-10-10 16:47:37 +0530

    Updates

---
+++ precache-manifest.c0505e017a9840b81c997c9617a51aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be54ac2c95376b4c7f6204a66062903",
+    "url": "/j-web/index.html"
+  },
+  {
+    "revision": "09715f4f12fbfd4684cf",
+    "url": "/j-web/static/css/main.7b68d56c.chunk.css"
+  },
+  {
+    "revision": "5f1d04f6c3a491a590fd",
+    "url": "/j-web/static/js/2.85ea4413.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/j-web/static/js/2.85ea4413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09715f4f12fbfd4684cf",
+    "url": "/j-web/static/js/main.ea572882.chunk.js"
+  },
+  {
+    "revision": "46a3c51b65b7c71f7b37",
+    "url": "/j-web/static/js/runtime-main.3098f62b.js"
+  }
+]);
